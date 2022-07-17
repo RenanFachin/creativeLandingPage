b781708b948b863c092e9ed7e008ef0d2ae17517
Renan Fachin <82840836+RenanFachin@users.noreply.github.com>
2022-07-17 01:38:45 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    let menuToggle = document.querySelector('.menuToggle')
+    let navigation = document.querySelector('nav')
+
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
+
